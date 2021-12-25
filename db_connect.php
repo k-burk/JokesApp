aff32bcae78c44d6f5aba078d3a302003b6c62bc
Kara <71410027+k-burk@users.noreply.github.com>
2021-12-24 23:25:04 -0700

    Updating mySQL connection info

--- db_connect.php
+++ db_connect.php
@@ -1,9 +1,9 @@
 <?php
 //using variables to connect to database
-$host = "localhost";
-$username = "root";
-$user_pass = "usbw";
-$database_in_use = "test";
+$host = "ble5mmo2o5v9oouq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	";
+$username = "p1pfxhov83hucezw";
+$user_pass = "yczlqimpgklsstdi";
+$database_in_use = "itpv8tgh57ojxwv9";
 //creating database connection instance
 $mysqli = new mysqli($host, $username, $user_pass, $database_in_use);
 
